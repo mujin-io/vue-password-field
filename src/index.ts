8c6593bdb706e6e70fa187311c2a28d245c91c15
Bill li <bill.li@ofcold.com>
2021-08-08 02:37:35 +0800

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {defineComponent, reactive, createVNode, watch} from 'vue';
+import {defineComponent, reactive, createVNode, watch, PropType} from 'vue';
 
 interface PasswordDataInterface {
 	isPlaintext: boolean;
@@ -9,7 +9,7 @@ interface PasswordDataInterface {
 
 const rules =  {
 	lower: /(.*[a-z].*)/,
-	upper: /(.*[a-z].*)/,
+	upper: /(.*[A-Z].*)/,
 	numberic: /(.*\d.*)/,
 	symbol: /[\x21-\x2F\x3A-\x3C\x3E-\x40\x5B\x5D-\x60\x7B\x7D-\x7E]/,
 };
@@ -52,7 +52,7 @@ const requirementRules: PasswordRequirementRuleInterface[] = [
 	{
 		key: 'notContainAccounts',
 		rule: (password: string, callback): boolean => typeof callback === 'function' && callback(password.toLowerCase()),
-		message: 'does not contain your account information.'
+		message: 'does not contain your account information'
 	}
 ];
 
@@ -67,16 +67,10 @@ export default defineComponent({
 		},
 		minimumCharacters: { type: Number, default: 8},
 		containAccounts: { type: Array, default: []},
-		trans: { type: Function, default: (k: string, locale?: string) => k}
+		trans: { type: Function as PropType<() => string>, default: (k: string, locale?: string) => k}
 	},
 	emits: ['update:modelValue'],
-	setup({
-		modelValue,
-		requirements,
-		minimumCharacters,
-		containAccounts,
-		trans
-	}, context) {
+	setup({modelValue, requirements, minimumCharacters, containAccounts, trans}, context) {
 		const data = reactive<PasswordDataInterface>({
 			isPlaintext: false,
 			isShowTips: false,
@@ -96,18 +90,18 @@ export default defineComponent({
 			let viewsList = requirementRules.filter(v => requirements.some((k:string) => k === v.key)).map(r => {
 				let isPassed = r.key !== 'notContainAccounts' ? r.rule(data.password)
 					: r.rule(data.password, (p: string) => {
-						return p.length > 0 && ! containAccounts.filter(v => v !== undefined || v !== '' || v !== null)
-							.map(v => v.toLowerCase())
-							.every(v => p.includes(v));
+						return p.length > 0 && ! containAccounts.filter((v): boolean => v !== undefined || v !== '' || v !== null)
+							.map((v): string => v.toString().toLowerCase())
+							.some((v: boolean) => p.includes(v));
 					});
 				return createVNode('li', {
-					class: 'flex items-center ' + (isPassed ? 'text-green-600' : 'text-red-600'),
+					class: 'flex items-start ' + (isPassed ? 'text-green-600' : 'text-red-600'),
 					'data-id': 'PasswordStrength-' + r.key
 				}, [
-					createVNode('span', {class: 'w-4 h-4 mr-2'}, [
+					createVNode('span', {class: 'w-3 h-3 mt-1 mr-2'}, [
 						createVNode('svg', {
 							viewBox: '0 0 48 48',
-							class: 'fill-current',
+							class: 'fill-current m-0',
 						}, [
 							createVNode('path', {
 								d: isPassed ? icons.passed : icons.failed
@@ -115,16 +109,16 @@ export default defineComponent({
 						])
 					]),
 					createVNode('span', {
-						class: 'text-sm'
+						class: 'flex-1 min-w-0'
 					}, trans(r.message.replace(':len', minimumCharacters)))
 				]);
 			});
-			viewsList.push(createVNode('li', {class: 'flex items-center text-sm text-gray-700', 'data-id': 'PasswordStrength-notContainAccounts'}, [
-				createVNode('span', {class: 'flex h-2 w-2 relative mr-2'}, [
+			viewsList.push(createVNode('li', {class: 'flex items-start items-center text-gray-700', 'data-id': 'PasswordStrength-notContainAccounts'}, [
+				createVNode('span', {class: 'flex h-2 w-2 relative mr-2', style: 'margin-left: 2px;'}, [
 					createVNode('span', {class: (data.isIncludes ? 'animate-ping': '') +' absolute inline-flex h-full w-full rounded-full bg-gray-600 opacity-75'}),
 					createVNode('span', {class: 'relative inline-flex rounded-full h-2 w-2 bg-gray-700'}),
 				]),
-				createVNode('span', {}, trans('is not commonly used')),
+				createVNode('span', {class: 'flex-1 min-w-0', style: 'margin-left: 2px;'}, trans('is not commonly used')),
 			]));
 
 			return viewsList;
@@ -138,7 +132,7 @@ export default defineComponent({
 					createVNode('input', {
 						type: data.isPlaintext ? 'text' : 'password',
 						value: data.password,
-						class: 'mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500',
+						class: 'mt-1 block w-full py-2 pr-3 border-0 border-b border-gray-300 bg-white focus:outline-none focus:ring-indigo-500 focus:border-green-700',
 						placeholder: '••••••••',
 						onKeyup: (event: KeyboardEvent) => {},
 						onInput: (event: InputEvent) => {
@@ -149,7 +143,7 @@ export default defineComponent({
 					}),
 
 					createVNode('button', {
-						class: 'm-0 ml-3 absolute inline-flex inset-y-0 right-0 flex items-center pr-2 cursor-pointer no-underline visible',
+						class: 'm-0 ml-3 absolute text-gray-700 inline-flex inset-y-0 right-0 flex items-center pr-2 cursor-pointer no-underline visible',
 						type: 'button',
 						onClick: (event: Event) => data.isPlaintext = !data.isPlaintext
 					}, [
@@ -170,14 +164,16 @@ export default defineComponent({
 							createVNode('path', {
 								d: 'M33.794 32.058L22.328 20.592A5.022 5.022 0 0 0 23.062 18a4.712 4.712 0 0 0-.174-1.2 2.625 2.625 0 0 1-2.221 1.278A2.667 2.667 0 0 1 18 15.417a2.632 2.632 0 0 1 1.35-2.27 4.945 4.945 0 0 0-1.35-.209 5.022 5.022 0 0 0-2.592.734L3.942 2.206a.819.819 0 0 0-1.157 0l-.578.579a.817.817 0 0 0 0 1.157l6.346 6.346c-3.816 2.74-6.3 6.418-6.3 8.072 0 3 7.458 10.7 15.686 10.7a16.455 16.455 0 0 0 7.444-1.948l6.679 6.679a.817.817 0 0 0 1.157 0l.578-.578a.818.818 0 0 0-.003-1.155zM18 27.225a9.2 9.2 0 0 1-7.321-14.811l2.994 2.994A5.008 5.008 0 0 0 12.938 18 5.062 5.062 0 0 0 18 23.063a5.009 5.009 0 0 0 2.592-.736l2.994 2.994A9.144 9.144 0 0 1 18 27.225z'
 							})
-						]) 
+						])
 					])
 				]
 			),
 
 			createVNode('section', {
+				class: 'pt-4 text-sm'
 			}, [
-				createVNode('ul', {}, messagesView())
+				createVNode('strong', {class: 'text-gray-800'}, [trans('Create a password that:')]),
+				createVNode('ul', {class: 'mt-2'}, messagesView())
 			])
 		]
 
